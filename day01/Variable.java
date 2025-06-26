39100106d27a0fa2d933509c8e3f24fa308a3201
flook25 <flook11223344@gmail.com>
2025-06-27 00:35:36 +0700

    commit day01

---
+++ day01/Variable.java
@@ -0,0 +1,24 @@
+public class Variable {
+
+    public static void main(String[] args) {
+        int myNumber = 10;
+        String myName = "John";
+        boolean isSunny = true;
+        float myFloat = 10.5f;
+        double myDouble = 10.5;
+        char myChar = 'A';
+        int[] myArray = {1 , 2, 3, 4, 5,};
+
+        System.out.println(myNumber);
+        System.out.println(myName);
+        System.out.println(isSunny);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+        System.out.println(myArray[0]);
+
+    }
+    
+}
+
+
